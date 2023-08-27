f624e5c81fb5806502bad8bf4aa08d85b3b22056
Joel <jcr4990@gmail.com>
2023-08-27 16:29:18 -0400

    Add -m argument to select model for upscale

--- eq-upscale.py
+++ eq-upscale.py
@@ -48,6 +48,7 @@ msg = "Welcome to eq-upscale. Use this tool to upscale texture images located in
 parser = argparse.ArgumentParser(description=msg)
 parser.add_argument("-s", "--scale", help="Upscale ratio (can be 2, 3, 4. default=4)", default="4", type=str)
 parser.add_argument("-t", "--texture_prefix", help="Comma separated texture prefix filter. Only upscale textures starting with these values. For example -t stone,rock will upscale all textures starting with 'stone' or 'rock'", default="", type=str)
+parser.add_argument("-m", "--model", help="Models included: realesrgan-x4plus, realesrgan-x4plus-anime, realesr-animevideov3-x2, realesr-animevideov3-x3, realesr-animevideov3-x4 (default=realesrgan-x4plus)", default="realesrgan-x4plus", type=str)
 args = parser.parse_args()
 
 if os.path.exists("archives") is False:
@@ -62,25 +63,26 @@ for archive in archives:
     # Extract s3d/eqg
     if archive.endswith(".s3d") or archive.endswith(".eqg"):
         archive_found = True
+        files_to_upscale = []
+        files_upscaled = 0
         result = subprocess.run(["quail", "extract", "archives//" + archive, "extracted//_" + archive], shell=True, capture_output=True, text=True,)
         print(result.stdout)
 
-        # Iterate files in archive folder and upscale with realesrgan
+        # Iterate files in archive to get count of files to be processed for progress reporting
         files = os.listdir("extracted//_" + archive)
-        files_to_process = 0
-        files_processed = 0
         for file in files:
             if (file.endswith(".dds") or file.endswith(".bmp") or file.endswith(".png")) and file.startswith(tuple(args.texture_prefix.split(","))):
-                files_to_process += 1
+                files_to_upscale.append(file)
 
         if len(args.texture_prefix) > 0:
-            print(f"{archive} contains {files_to_process} images to process. (filtered by prefix: {args.texture_prefix})\n")
+            print(f"{archive} contains {len(files_to_upscale)} images to process. (filtered by prefix: {args.texture_prefix})\n")
         else:
-            print(f"{archive} contains {files_to_process} images to process.\n")
+            print(f"{archive} contains {len(files_to_upscale)} images to process.\n")
 
-        for file in files:
+        # Iterate files in archive and upscale with realesrgan
+        for file in files_to_upscale:
             if (file.endswith(".dds") or file.endswith(".bmp") or file.endswith(".png")) and file.startswith(tuple(args.texture_prefix.split(","))):
-                files_processed += 1
+                files_upscaled += 1
                 original_path = "extracted//_" + archive + "//" + file
                 upscaled_path = "tmp//" + file.split(".")[0] + ".png"
                 img_format = get_format(original_path)
@@ -88,14 +90,14 @@ for archive in archives:
                 if img_format == "bmp":
                     if transparency_check(original_path) is True:
                         print(f"Skipping texture with transparency: {file}")
-                        print(f"Progress: {files_processed}/{files_to_process}\n")
+                        print(f"Progress: {files_upscaled}/{len(files_to_upscale)}\n")
                         continue
 
                 print(f"File: {file}\nType: {img_format}")
-                result = subprocess.run(["realesrgan-ncnn-vulkan.exe", "-i", original_path, "-o", upscaled_path, "-n", "realesrgan-x4plus", "-s", args.scale], shell=True, capture_output=True, text=True,)
+                result = subprocess.run(["realesrgan-ncnn-vulkan.exe", "-i", original_path, "-o", upscaled_path, "-n", args.model, "-s", args.scale], shell=True, capture_output=True, text=True,)
 
                 save_upscaled_img(img_format, original_path, upscaled_path)
-                print(f"Progress: {files_processed}/{files_to_process}\n")
+                print(f"Progress: {files_upscaled}/{len(files_to_upscale)}\n")
 
         # Compress upscaled images back into archive
         result = subprocess.run(["quail", "compress", "extracted//_" + archive], shell=True, capture_output=True, text=True,)
